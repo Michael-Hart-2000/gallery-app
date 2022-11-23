f40c0b576c841a34d46037ec02a30659376dc8d1
Michael-Hart-2000 <60468959+michael-hart-2000@users.noreply.github.com>
2022-11-23 15:45:13 +0000

    Adding new model class called Artist

---
+++ README.md
@@ -0,0 +1,12 @@
+# gallery-app
+Console Gallery App written in Kotlin
+
+## Project Description
+
+## Project Purpose
+
+## Getting Started with the Project
+
+## Getting Help
+
+## Project Authors / Contributors
---
+++ src/main/kotlin/models/Artist.kt
@@ -0,0 +1,11 @@
+package models
+
+data class Artist(
+    val artistName: String,
+    val artistAge: Int,
+    val artistCountry: String,
+    val artistMovement: String,
+    val artistPopularity: Int,
+    val isArtistAlive: Boolean
+){
+}
