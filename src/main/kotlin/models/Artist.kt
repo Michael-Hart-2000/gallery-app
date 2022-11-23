090185a73690e743da10c553d7e01e11a2962790
20089696 <20089696@mail.wit.ie>
2022-11-23 16:41:08 +0000

    Adding new model class called Artist

---
+++ src/main/kotlin/models/Artist.kt
@@ -0,0 +1,9 @@
+package models
+
+data class Note(val artistName: String,
+                val artistAge: Int,
+                val artistCountry: String,
+                val artistMovement: String,
+                val artistPopularity: Int,
+                val isArtistAlive: Boolean){
+}
